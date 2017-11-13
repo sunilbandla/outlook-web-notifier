df82eee3c547fef74669906496e7bd3b2c488db9
Sunil Bandla <sunilbandla@gmail.com>
2017-11-12 19:25:47 -0800

    Reformat files

--- scripts/auth.js
+++ scripts/auth.js
@@ -3,33 +3,33 @@
 'use strict';
 
 let ADAL = new AuthenticationContext({
-    instance: 'https://login.microsoftonline.com/',
-    tenant: 'common', // COMMON OR YOUR TENANT ID
+  instance: 'https://login.microsoftonline.com/',
+  tenant: 'common', // COMMON OR YOUR TENANT ID
 
-    clientId: 'e8c8e28c-ab9f-4c7a-904a-e64443f372b5',
-    redirectUri: 'http://127.0.0.1:8887/',
+  clientId: 'e8c8e28c-ab9f-4c7a-904a-e64443f372b5',
+  redirectUri: 'http://127.0.0.1:8887/',
 
-    callback: userSignedIn,
-    popUp: true
+  callback: userSignedIn,
+  popUp: true
 });
 
 function signIn() {
-    ADAL.login();
+  ADAL.login();
 }
 
 function userSignedIn(err, token) {
-    console.log('userSignedIn called');
-    if (!err) {
-        console.log('token: ' + token);
-        showWelcomeMessage();
-    }
-    else {
-        console.error('error: ' + err);
-    }
+  console.log('userSignedIn called');
+  if (!err) {
+    console.log('token: ' + token);
+    showWelcomeMessage();
+  }
+  else {
+    console.error('error: ' + err);
+  }
 }
 
 function showWelcomeMessage() {
-    let user = ADAL.getCachedUser();
-    let divWelcome = document.getElementById('WelcomeMessage');
-    divWelcome.innerHTML = 'Welcome ' + user.profile.name;
+  let user = ADAL.getCachedUser();
+  let divWelcome = document.getElementById('WelcomeMessage');
+  divWelcome.innerHTML = 'Welcome ' + user.profile.name;
 }
